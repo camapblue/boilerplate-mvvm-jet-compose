3044291d3419549673d953bf47a37d5fd06e9393
@camapblue <lequangdao.itm@gmail.com>
2022-02-10 17:31:49 +0700

    Update data analytics & api

--- repository/src/main/java/ptc/tech/repository/api/BaseApi.kt
+++ repository/src/main/java/ptc/tech/repository/api/BaseApi.kt
@@ -2,17 +2,25 @@ package ptc.tech.repository.api
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONObject
 import org.json.JSONTokener
+import ptc.tech.repository.global.DataAnalytics
 import java.net.URL
 
-open class BaseApi {
-    fun get(path: String): JSONObject {
+open class BaseApi(
+    private val analytics: DataAnalytics = DataAnalytics.instance
+) {
+    private fun execute(method: String, path: String, params: JSONObject? = null): JSONObject {
+        // track api
+        analytics.trackApi(path, "GET", params)
         try {
             val url = URL(path)
             val client = OkHttpClient()
 
+            val body = params?.toString()?.toRequestBody()
             val request = Request.Builder()
+                .method(method, body)
                 .url(url)
                 .build()
 
@@ -21,8 +29,17 @@ open class BaseApi {
 
             return JSONTokener(json).nextValue() as JSONObject
         } catch (e: Exception) {
-            println("Api Error: $e")
+            // track api exception
+            analytics.trackApiError(path, e)
             throw e
         }
+    }
+
+    fun get(path: String): JSONObject {
+        return execute("GET", path)
+    }
+
+    fun post(path: String, params: JSONObject = JSONObject()): JSONObject {
+        return execute("POST", path, params)
     }
 }
